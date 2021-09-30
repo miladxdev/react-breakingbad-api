81989e63f09fe6e05ceb04a6aaaf532613122689
Milad <miladxgh@outlook.com>
2021-09-30 15:14:04 +0330

    footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
-
+import logo from '../img/logo.png'
+import instagram from '../img/Circled_Instagram.png'
+import linkedin from '../img/Circled_Linkedin.png'
+import twitter from '../img/Circled_Twitter.png'
+import youtube from '../img/Circled_Youtube.png'
 const Footer = () => {
     return (
         <footer>
             <div className="page-wrapper">
-                <h1>Breaking Bad</h1>
+              <div className="footer-row">
+                  <div className="footer-col">
+                    <img src={logo} alt="logo" className="footer-logo" />
+                  </div>
+                
+                <div className="footer-col">
+                   <p>Walter White, a chemistry teacher, discovers that he has cancer and decides to get into the meth-making business to repay his medical debts. His priorities begin to change when he partners with Jesse.                </p> 
+                </div>
+
+                <div className="footer-col">
+                    <h3>First episode date:</h3> 
+                    <p>January 20, 2008</p>
+
+                    <h3>Created by:</h3> 
+                    <p>Vince Gilligan</p>  
+                </div>
+
+                <div className="footer-col">
+                    <p>Better Call Saul, a prequel series featuring Odenkirk, Banks, and Esposito reprising their Breaking Bad roles, debuted on AMC on February 8, 2015, and has been renewed for a final sixth season. </p>
+                </div>
+              </div>
+
+                <div className="social">
+                    <img src={instagram} alt="logo" width="50"/>             
+                    <img src={linkedin} alt="logo" width="50"/>             
+                    <img src={twitter} alt="logo" width="50"/>             
+                    <img src={youtube} alt="logo" width="50"/>             
+                </div>
             </div>
         </footer>
     )
